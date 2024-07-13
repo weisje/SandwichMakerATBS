ddc442c70a74bffadb3790b773d0d190c616f171
weisje <20269574+weisje@users.noreply.github.com>
2024-07-13 07:24:29 -0400

    Added initial framing for breadSelector(), proteinSelector(), cheeseSelector(), toppingSelector(), and main() functions.

---
+++ sandwichMaker.py
@@ -0,0 +1,41 @@
+import pyinputplus
+
+
+def breadSelector() -> dict: # TODO
+    """
+    Gather's user information on their bread selection.  Returns selection to caller along with a price for the bread.
+    :return: dict
+    """
+    pass
+
+
+def proteinSelector() -> dict: # TODO
+    """
+    Gather's user information on their protein selection.  Returns selection to caller along with a price for the item.
+    :return: dict
+    """
+    pass
+
+
+def cheeseSelector() -> dict: # TODO
+    """
+    Gather's user information on their cheese selection.  Returns selection to caller along with a price for the item.
+    :return: dict
+    """
+    pass
+
+
+def toppingSelector() -> dict: # TODO
+    """
+    Gather's user information on their toppings selection.  Returns selection to caller along with a price for the items.
+    :return: dict
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
